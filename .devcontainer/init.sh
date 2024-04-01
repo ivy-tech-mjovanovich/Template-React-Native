f706061e7fbab8305c249151c39f6245c0bdf734
Jonny Danger <mpjovanovich@gmail.com>
2024-04-01 13:14:39 -0400

    initial commit

---
+++ .devcontainer/init.sh
@@ -0,0 +1,12 @@
+## All of these commands were run to create this template:
+# sudo npm install create-expo-app --global
+# sudo npx create-expo-app -t expo-template-blank-typescript ReactNativeApp
+# cd ReactNativeApp
+# sudo npx expo install react-native-web react-dom @expo/metro-runtime
+
+## Now that the app is set up, we just have to run the following:
+cd ReactNativeApp
+sudo npm install;
+
+## To clone without the whole commit history:
+# git clone https://github.com/ivy-tech-mjovanovich/Template-React-Native.git --depth 1
---
+++ ReactNativeApp/App.tsx
@@ -0,0 +1,13 @@
+// imports go here
+
+/*
+export default function App() {
+  return (
+  );
+}
+*/
+
+/*
+const styles = StyleSheet.create({
+});
+*/
